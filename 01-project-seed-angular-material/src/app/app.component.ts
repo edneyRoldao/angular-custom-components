f29f4844b0aa841c4ed28f6fe88b5c89c3d6fa3a
Edy <edneyroldao@gmail.com>
2019-05-25 19:24:57 -0300

    project seed created

---
+++ 01-project-seed-angular-material/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'root',
+    template: `
+        
+        <div> Project base is working properly </div>
+        
+    `
+})
+export class AppComponent {
+
+
+
+
+}
---
+++ 01-project-seed-angular-material/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
